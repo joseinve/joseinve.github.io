77a116f7dc32bbe206062700c427305ff02c149f
joseinve <116449272+joseinve@users.noreply.github.com>
2022-11-23 12:36:36 -0300

    Add files via upload

---
+++ incersion_datos.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <form action="carga_datos.php" method="POST">
+        <input type="text" placeholder="Contratante" name="contratante" required><br>
+        <input type="text" placeholder="Locales" name="local" required><br>
+        <input type="date" placeholder="Fecha" name="fecha" required><br>
+        <input type="text" placeholder="Evento" name="evento" required><br>
+        <input type="text" placeholder="cantidad de personas" name="personas" required><br>
+        <input type="submit" name="enviar">
+    </form>
+</body>
+
+</html>
